17edc0c894c2c7bf48207b74d86e75629827b598
Jacuqueline_Hsu <hsufuchiung@gmail.com>
2022-10-22 11:41:25 +0100

    Remove main storyboard

---
+++ Devskiller/Application/SceneDelegate.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        self.window = UIWindow(windowScene: windowScene)
+        setUpMainWindow()
+    }
+
+    private func setUpMainWindow() {
+        let vc = ViewController()
+        window?.rootViewController = vc
+        window?.makeKeyAndVisible()
+    }
+}
+
--- Devskiller/ViewController.swift
+++ Devskiller/ViewController.swift
@@ -1,20 +1,10 @@
-//
-//  ViewController.swift
-//  Devskiller
-//
-//  Created by Ivo Silva on 18/09/2020.
-//  Copyright © 2020 Mindera. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .yellow
     }
-
-
 }
 
