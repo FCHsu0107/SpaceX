e95420fe464c87025d0fee9771d64845de993a27
Jacuqueline_Hsu <hsufuchiung@gmail.com>
2022-10-27 14:31:54 +0100

    Remove unless code

--- Devskiller/SpeceX/API/CompanyProvider.swift
+++ Devskiller/SpeceX/API/CompanyProvider.swift
@@ -2,52 +2,15 @@ import Foundation
 import Promises
 
 protocol CompanyProviderProtocol {
-    func fetchCompany(completion: @escaping (Result<APIModel.Company, HTTPClientError>) -> Void)
-    func fetchLaunches(completion: @escaping (Result<APIModel.Launches, HTTPClientError>) -> Void)
     func fetchCompany2() -> Promise<APIModel.Company>
     func fetchLaunches2() -> Promise<APIModel.Launches>
 }
 
-class CompanyProvider: CompanyProviderProtocol {
+final class CompanyProvider: CompanyProviderProtocol {
     private let client: HTTPClient
     
     init(client: HTTPClient = HTTPClient()) {
         self.client = client
-    }
-    
-    func fetchCompany(completion: @escaping (Result<APIModel.Company, HTTPClientError>) -> Void) {
-        client.request(CompanyRequest()) { result in
-            switch result {
-            case .success(let data):
-                do {
-                    let company = try JSONDecoder().decode(APIModel.Company.self, from: data)
-                    completion(.success(company))
-                } catch {
-                    completion(.failure(.decodeError(message: error.localizedDescription)))
-                }
-                
-            case .failure(let error):
-                completion(.failure(error))
-            }
-        }
-    }
-    
-    func fetchLaunches(completion: @escaping (Result<APIModel.Launches, HTTPClientError>) -> Void) {
-        client.request(AllLaunchesRequest()) { result in
-            switch result {
-            case .success(let data):
-                do {
-                    let launches = try JSONDecoder().decode(APIModel.Launches.self, from: data)
-                    completion(.success(launches))
-                } catch {
-                    print("error \(error)")
-                    completion(.failure(.decodeError(message: error.localizedDescription)))
-                }
-                
-            case .failure(let error):
-                completion(.failure(error))
-            }
-        }
     }
     
     func fetchCompany2() -> Promise<APIModel.Company> {
